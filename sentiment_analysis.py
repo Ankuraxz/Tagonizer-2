4113acda5a711b70cf0e5fba7b8600ae1b7d8941
ANKUR <ankurvermaaxz@gmail.com>
2021-02-17 21:46:02 +0530

    Single Comment

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -53,37 +53,44 @@ def sentiment_analysis_with_opinion_mining_example(documents,client):
 
     for document in doc_result:
         print("Document Sentiment: {}".format(document.sentiment))
-        print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
-            document.confidence_scores.positive,
-            document.confidence_scores.neutral,
-            document.confidence_scores.negative,
-        ))
+        # print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+        #     document.confidence_scores.positive,
+        #     document.confidence_scores.neutral,
+        #     document.confidence_scores.negative,
+        # ))
         for sentence in document.sentences:
-            print("Sentence: {}".format(sentence.text))
-            print("Sentence sentiment: {}".format(sentence.sentiment))
-            print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
-                sentence.confidence_scores.positive,
-                sentence.confidence_scores.neutral,
-                sentence.confidence_scores.negative,
-            ))
+            # print("Sentence: {}".format(sentence.text))
+            # print("Sentence sentiment: {}".format(sentence.sentiment))
+            # print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+            #     sentence.confidence_scores.positive,
+            #     sentence.confidence_scores.neutral,
+            #     sentence.confidence_scores.negative,
+            # ))
             for mined_opinion in sentence.mined_opinions:
                 aspect = mined_opinion.aspect
+                opinions=[]
                 print("......'{}' aspect '{}'".format(aspect.sentiment, aspect.text))
-                print("......Aspect score:\n......Positive={0:.2f}\n......Negative={1:.2f}\n".format(
-                    aspect.confidence_scores.positive,
-                    aspect.confidence_scores.negative,
-                ))
+                # print("......Aspect score:\n......Positive={0:.2f}\n......Negative={1:.2f}\n".format(
+                #     aspect.confidence_scores.positive,
+                #     aspect.confidence_scores.negative,
+                # ))
                 for opinion in mined_opinion.opinions:
-                    print("......'{}' opinion '{}'".format(opinion.sentiment, opinion.text))
-                    print("......Opinion score:\n......Positive={0:.2f}\n......Negative={1:.2f}\n".format(
-                        opinion.confidence_scores.positive,
-                        opinion.confidence_scores.negative,
-                    ))
-            print("\n")
-        print("\n")
+                    opinions.append(opinion.text)
+
+                #     print("......'{}' opinion '{}'".format(opinion.sentiment, opinion.text))
+                #     print("......Opinion score:\n......Positive={0:.2f}\n......Negative={1:.2f}\n".format(
+                #         opinion.confidence_scores.positive,
+                #         opinion.confidence_scores.negative,
+                #     ))
+                #print(opinions)
+                reviews.update({str(aspect.text):(str(aspect.sentiment),opinions)})
+        #     print("\n")
+        # print("\n")
 
 documents = [ "Dont buy this product. Image quality is below average comparing with the specification. Useless camera. And there is NO RETURN POLICY and replacing with same mobile is big headache .DON’T buy this mobile"
 
     ]
 #print(type(documents))
 sentiment_analysis_with_opinion_mining_example(documents,client)
+
+print(reviews)
