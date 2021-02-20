636925082e160a9d4c0ef967beb2fab7086d932a
Rishabh Jain <32129398+jrishabh720@users.noreply.github.com>
2021-02-20 12:40:01 +0530

    UI Improved

--- web/pop_up/popup.js
+++ web/pop_up/popup.js
@@ -6,63 +6,87 @@
 //     },
 //     tags:{
 //         "D dsfsgdnggsfgdgdf":1,
-//         "E dsfsdgfdfs":2,
-//         "F fsddfghgsegh":0
+//         "E dsfsdgfdfs":0,
+//         "F fsddfghgsegh":2
 //     }
 // }
 
+// let text = "jbhjhj hghjvghc hvgfy hgvyfy jbjkbjkdkw hknkqsnkjqwbbdjwdb kdbwjkjbd";
+// document.getElementById(content).innerHTML(`<p> ${text.substring(100)} ...<p> ` )
+
 var foo = localStorage.getItem("data");
-console.log(foo);
-//data = foo.json() ;
+// console.log(foo);
 
 var review = localStorage.getItem("reviews");
 review = JSON.parse(review)
 
 
 data = JSON.parse(foo);
-console.log(data);
-console.log("running")
+// console.log(data);
+// console.log("running")
 document.getElementById("tags").addEventListener("click" , tags);
 document.getElementById("reviews").addEventListener("click" , reviews);
 
 function tags(){
-    let s = "";
+    let p ="";
+    let n ="";
+    let m ="";
+
     Object.keys(data.tags).forEach(function (key) {
         let val = data.tags[key];
         if(val==0){
-            s += "<div class='bg-danger rounded' style='color:white;width:400px'>";
-            s += key;
-            s += "</div>";
+            n += "<div class='tags badge badge-danger'>";
+            n += key;
+            n += "</div>";
+           
         }else if(val==1){
-            s += "<div class='bg-success rounded'style='color:white;width:400px'>";
-            s += key;
-            s += "</div>";
+            p += "<div class='tags badge badge-success'>";
+            p += key;
+            p += "</div>";
+           
         }else{
-            s += "<div class='bg-warning text-dark rounded' style='width:400px'>";
-            s += key;
-            s += "</div>";
+            m += "<div class='tags badge badge-info'>";
+            m += key;
+            m += "</div>";
+            
         }
       });
+    //   console.log(n);
+      let s="<div class='card-footer'>Positive</div>";
+      s += p;
+      s += "<br>Moderate<br>";
+      s += m;
+      s += "<br>Negative<br>";
+      s += n;
       document.getElementById("out").innerHTML = s;
 }
 
 function reviews(){
-    let s = "";
+    let s ="";
+    let p ="";
+    let n ="";
+    let m ="";
     Object.keys(data.reviews).forEach(function (key) {
         let val = data.reviews[key];
         if(val==0){
-            s += "<div class='bg-danger rounded' style='color:white;width:400px'>";
-            s += review[key];
-            s += "</div>";
+            n += "<div class='tags badge badge-danger'>";
+            n += review[key];
+            n += "</div>";
         }else if(val==1){
-            s += "<div class='bg-success rounded'style='color:white;width:400px'>";
-            s += review[key];
-            s += "</div>";
+            p += "<div class='tags badge badge-success'>";
+            p += review[key];
+            p += "</div>";
         }else{
-            s += "<div class='bg-warning text-dark rounded' style='width:400px'>";
-            s += review[key];
-            s += "</div>";
+            m += "<div class='tags badge badge-info'>";
+            m += review[key];
+            m += "</div>";
         }
       });
+      s="<div class='card-footer'>Positive</div>";
+      s += p;
+      s += "<br>Moderate<br>";
+      s += m;
+      s += "<br>Negative<br>";
+      s += n;
       document.getElementById("out").innerHTML = s;
 }
