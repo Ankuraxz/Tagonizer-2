75df54da8da08f97544d62be137b3fe6d77fbe89
Sonali Singh <sonali122000@gmail.com>
2021-03-04 23:17:07 +0530

    new changes

--- web/content.js
+++ web/content.js
@@ -1,8 +1,28 @@
-
-
+// let loop = 2;
 let link = document.querySelector("#reviews-medley-footer a");
 const url = link.href;
 let arr = [];
+let sellerImages = [];
+let customerImages = [];
+let api = "https://tagonizer.herokuapp.com";
+
+// let ratings = document
+//   .getElementById("acrCustomerReviewText")
+//   .innerText.replace(" ratings", "");
+// ratings = parseInt(ratings.replace(/,/g, ""));
+// console.log(ratings);
+
+// let runLoop = loop < ratings / 10;
+
+
+/***  Get seller images ***/
+const seller = document.querySelectorAll(".imageThumbnail .a-button-text img");
+seller.forEach((ele) =>
+  sellerImages.push(ele.getAttribute("src"))
+);
+console.log(sellerImages);
+
+
 
 async function getReviews(url) {
   const res = await fetch(url);
@@ -11,29 +31,41 @@ async function getReviews(url) {
   let parser = new DOMParser();
   let doc = parser.parseFromString(text, "text/html");
 
+ /**  Get reviews **/
   let content = doc.querySelector("#cm_cr-review_list");
   let parent = content.getElementsByClassName("a-section review aok-relative");
 
   for (i = 0; i < parent.length; i++) {
     let reviewDiv = parent[i].children[0].children[0];
-
     let child = reviewDiv.children[4].children[0];
     let string = child.innerText.replace(/(\r\n|\n|\r)/gm, "");
-    //arr.push(child.innerText.trim());
     arr.push(string.trim());
   }
 
+  /** Get images **/
+  const imgArr = document.querySelectorAll(
+    ".review-image-tile-section .review-image-tile"
+  );
+
+  imgArr.forEach((ele) => {
+    customerImages.push(ele.getAttribute("src"));
+    console.log(ele.getAttribute("src"));
+  });
 
 
   postData(api, arr);
+ // postImage(api);
+ 
 
+ 
 
 }
 
-let api = "https://tagonizer.herokuapp.com/predict";
 
-async function postData(url, data) {
-  const response = await fetch(url, {
+async function postData(url, data ) {
+
+  //Revies api
+  const response = await fetch(url + "/predict", {
     method: "POST",
     mode: "cors",
     headers: {
@@ -42,14 +74,46 @@ async function postData(url, data) {
     body: JSON.stringify({ comments: data }),
   });
 
-  const values = await response.json();
+ const values = await response.json();
+  console.log(values);
 
+  //Image api
+  const res = await fetch(url + "/image", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      seller_img: sellerImages,
+      customer_img: customerImages,
+    }),
+  });
+
+ const imgResponse = await res.json();
+  console.log(imgResponse);
 
   chrome.runtime.sendMessage({
-    data: values, // or whatever you want to send
-    reviews : arr
+    data: values,
+    reviews: arr,
+    images: imgResponse
   });
+
+
 }
 
-getReviews(url);
+// async function fetchData() {
+//   let ans = await getReviews(url);
 
+  //  while(runLoop && loop<=4){
+  // let ans= await getReviews(`https://www.amazon.in/T-Rock-Mens-Running-Shoes/product-reviews/B0886F4DHY/ref=cm_cr_arp_d_paging_btm_2?ie=UTF8&pageNumber=${loop}&reviewerType=all_reviews`)
+  //   runLoop = loop < (ratings/10)
+  //   loop++;
+  // }
+
+  // console.log(customerImages);
+  // console.log(arr);
+
+
+//}
+
+getReviews(url);
--- web/pop_up/popup.js
+++ web/pop_up/popup.js
@@ -1,219 +1,231 @@
-let work =true;
+let browserData;
+let data = null;
+let review = null;
+let imgData = null;
+
 function callback(tabs) {
-    var currentTab = tabs[0].url; 
-  
-  if(!currentTab.startsWith("https://www.amazon.in/")){
-      document.getElementsByClassName("loader")[0].style.display="none";
-      document.getElementsByClassName("invalid")[0].style.display="block";
-      work = false;
+  var currentTab = tabs[0].url;
+  if (!currentTab.startsWith("https://www.amazon.in/")) {
+    document.getElementsByClassName("loader")[0].style.display = "none";
+    document.getElementsByClassName("invalid")[0].style.display = "block";
+  } else {
+    document.getElementsByClassName("loader")[0].style.display = "block";
+    document.getElementsByClassName("invalid")[0].style.display = "none";
   }
+}
 
-  else{
-    document.getElementsByClassName("loader")[0].style.display="block";
-    document.getElementsByClassName("invalid")[0].style.display="none";
-    work = true;
-  }
-  }
-  let query = { active: true, currentWindow: true };
-
-
-
-var foo = localStorage.getItem("data");
-// console.log(foo);
-
-var review = localStorage.getItem("reviews");
-review = JSON.parse(review)
-localStorage.clear();
-data = JSON.parse(foo);
-
-document.getElementById("tags").addEventListener("click" , tags);
-document.getElementById("reviews").addEventListener("click" , reviews);
-
-
-
+let query = { active: true, currentWindow: true };
 
 document.onreadystatechange = function () {
-    
-   chrome.tabs.query(query, callback);
-  document.getElementById('main').style.display="none";
-  document.getElementById('home').style.display="block";
-         
- if(work && data!=null){
-        setTimeout(function(){
-           document.getElementById('home').style.display="none";
-           document.getElementById('main').style.display="block";
-           tags();
-        },1000);
+  chrome.tabs.query(query, callback);
+  document.getElementById("main").style.display = "none";
+  document.getElementById("home").style.display = "block";
+};
+
+/*** Receive data from background.js ***/
+function getData(print) {
+  console.log("running...");
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    browserData = request;
+    data = browserData.data;
+    review = browserData.reviews;
+    imgData = browserData.images;
+    //console.log(browserData);
+    print(browserData);
+  });
+}
+
+function print(browserData) {
+  console.log(browserData);
+  // console.log(browserData)
+  document.getElementById("home").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  tags();
+}
+
+getData(print);
+
+// var foo = localStorage.getItem("data");
+// var review = localStorage.getItem("reviews");
+// var imgLinks = localStorage.getItem("images");
+
+// review = JSON.parse(review);
+// data = JSON.parse(foo);
+// imgData = JSON.parse(imgLinks) ;
+
+// localStorage.clear();
+
+document.getElementById("tags").addEventListener("click", tags);
+document.getElementById("reviews").addEventListener("click", reviews);
+document.getElementById("images").addEventListener("click", images);
+
+//dark theme toggler
+const checkbox = document.querySelectorAll(".checkbox").forEach((ele) =>
+  ele.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+  })
+);
+
+function tags() {
+  let p = "";
+  let n = "";
+  let m = "";
+
+  document.getElementById("negative").innerHTML = "";
+  document.getElementById("positive").innerHTML = "";
+  document.getElementById("mixed").innerHTML = "";
+  document.getElementById("imgBox").innerHTML = "";
+
+  Object.keys(data.tags).forEach(function (key) {
+    let val = data.tags[key];
+    if (val == 0) {
+      n = "<div class='tags badge badge-danger'>";
+      n += key;
+      n += "</div>";
+      if (document.getElementById("negative").innerHTML === "") {
+        document
+          .getElementById("negative")
+          .insertAdjacentHTML("beforeend", "<h6>Negative</h6>");
+      }
+      document.getElementById("negative").insertAdjacentHTML("beforeend", n);
+    } else if (val == 1) {
+      p = "<div class='tags badge badge-success'>";
+      p += key;
+      p += "</div>";
+      if (document.getElementById("positive").innerHTML === "") {
+        document
+          .getElementById("positive")
+          .insertAdjacentHTML("beforeend", "<h6>Positive</h6>");
+      }
+      document.getElementById("positive").insertAdjacentHTML("beforeend", p);
+    } else {
+      m += "<div class='tags badge badge-info'>";
+      m += key;
+      m += "</div>";
+      if (document.getElementById("mixed").innerHTML === "") {
+        document
+          .getElementById("mixed")
+          .insertAdjacentHTML("beforeend", "<h6>Mixed</h6>");
+      }
+      document.getElementById("mixed").insertAdjacentHTML("beforeend", m);
     }
-    
-  }
-
-
-function tags(){
-
-        let p ="";
-        let n ="";
-        let m ="";
-    
-        document.getElementById("negative").innerHTML="";
-        document.getElementById("positive").innerHTML="";
-        document.getElementById("mixed").innerHTML="";
-
-        Object.keys(data.tags).forEach(function (key) {
-            let val = data.tags[key];
-            if(val==0){
-                n = "<div class='tags badge badge-danger'>";
-                n += key;
-                n += "</div>";
-                if(document.getElementById("negative").innerHTML===""){
-                    document.getElementById("negative").insertAdjacentHTML('beforeend',"<h6>Negative</h6>");
-                }
-                document.getElementById("negative").insertAdjacentHTML('beforeend',n);
-            
-            }else if(val==1){
-                p = "<div class='tags badge badge-success'>";
-                p += key;
-                p += "</div>";
-                if(document.getElementById("positive").innerHTML===""){
-                    document.getElementById("positive").insertAdjacentHTML('beforeend',"<h6>Positive</h6>");
-                }
-                document.getElementById("positive").insertAdjacentHTML('beforeend',p);
-            
-            }else{
-                m += "<div class='tags badge badge-info'>";
-                m += key;
-                m += "</div>";
-                if(document.getElementById("mixed").innerHTML===""){
-                    document.getElementById("mixed").insertAdjacentHTML('beforeend',"<h6>Mixed</h6>");
-                }
-                document.getElementById("mixed").insertAdjacentHTML('beforeend',m);
-            }
-        });
-   
+  });
 }
 
-function reviews(){
-    let s ="";
-    let p ="";
-    let n ="";
-    let m ="";
-   
-    
-    document.getElementById("negative").innerHTML="";
-    document.getElementById("positive").innerHTML="";
-    document.getElementById("mixed").innerHTML="";
-    
-    Object.keys(data.reviews).forEach(function (key) {
-   
-        let val = data.reviews[key];
-        let divId = "review"+key;
-         let trimmedText = review[key].split(" ").splice(0,10).join(" ");
-        // let remainingString = review[key].split(" ").splice(6).join()
-        if(val==0){
-            n = "<div  id=";
-            n+=divId;
-            n+= " class='review-div negative'>";
-            n += trimmedText;
-            n += "</div>";
+function reviews() {
+  let p = "";
+  let n = "";
+  let m = "";
 
-           
-       
-            if(document.getElementById("negative").innerHTML===""){
-                document.getElementById("negative").insertAdjacentHTML('beforeend',"<h6>Negative Reviews</h6>");
-            }
+  document.getElementById("negative").innerHTML = "";
+  document.getElementById("positive").innerHTML = "";
+  document.getElementById("mixed").innerHTML = "";
+  document.getElementById("imgBox").innerHTML = "";
 
-           document.getElementById("negative").insertAdjacentHTML('beforeend',n);
-           if(review[key].split(" ").length > 10){
-            document.getElementById(divId).insertAdjacentHTML('beforeend',"<span>...</span>")
+  Object.keys(data.reviews).forEach(function (key) {
+    let val = data.reviews[key];
+    let divId = "review" + key;
+    let trimmedText = review[key].split(" ").splice(0, 10).join(" ");
+
+    if (val == 0) {
+      n = "<div  id=";
+      n += divId;
+      n += " class='review-div negative'>";
+      n += trimmedText;
+      n += "</div>";
+
+      if (document.getElementById("negative").innerHTML === "") {
+        document
+          .getElementById("negative")
+          .insertAdjacentHTML("beforeend", "<h6>Negative Reviews</h6>");
+      }
+
+      document.getElementById("negative").insertAdjacentHTML("beforeend", n);
+      if (review[key].split(" ").length > 10) {
+        document
+          .getElementById(divId)
+          .insertAdjacentHTML("beforeend", "<span>...</span>");
+      }
+    } else if (val == 1) {
+      p = "<div id=";
+      p += divId;
+      p += " class='review-div positive'>";
+      p += trimmedText;
+      p += "</div>";
+
+      if (document.getElementById("positive").innerHTML === "") {
+        document
+          .getElementById("positive")
+          .insertAdjacentHTML("beforeend", "<h6>Positive Reviews</h6>");
+      }
+
+      document.getElementById("positive").insertAdjacentHTML("beforeend", p);
+      if (review[key].split(" ").length > 10) {
+        document
+          .getElementById(divId)
+          .insertAdjacentHTML("beforeend", "<span>...</span>");
+      }
+    } else {
+      m = "<div id=";
+      m += divId;
+      m += " class='review-div mixed'>";
+      m += trimmedText;
+      m += "</div>";
+
+      if (document.getElementById("mixed").innerHTML === "") {
+        document
+          .getElementById("mixed")
+          .insertAdjacentHTML("beforeend", "<h6>Moderate Reviews</h6>");
+      }
+      document.getElementById("mixed").insertAdjacentHTML("beforeend", m);
+      if (review[key].split(" ").length > 10) {
+        document
+          .getElementById(divId)
+          .insertAdjacentHTML("beforeend", "<span>...</span>");
+      }
+    }
+
+    document.getElementById(divId).addEventListener("click", function () {
+      this.classList.toggle("show");
+      if (this.classList.contains("show")) {
+        this.innerText = review[key];
+      } else {
+        let prevValue = review[key].split(" ").splice(0, 10).join(" ");
+        this.innerText = prevValue;
+        if (review[key].split(" ").length > 10) {
+          this.insertAdjacentHTML("beforeend", "<span>...</span>");
         }
-          
-            document.getElementById(divId).addEventListener("click" , function(){
-                this.classList.toggle("show");
-                if(this.classList.contains("show")){
-                    this.innerText= review[key];
-                }
-
-                else{
-                    let prevValue = review[key].split(" ").splice(0,10).join(" ");
-                    this.innerText= prevValue;
-                    if(review[key].split(" ").length > 10){
-                        this.insertAdjacentHTML('beforeend',"<span>...</span>")
-                    }
-                }
-              
-            })
-
-           
-        }else if(val==1){
-            p = "<div id=";
-            p +=divId;
-            p += " class='review-div positive'>";
-            p += trimmedText;
-            p += "</div>";
-
-            if(document.getElementById("positive").innerHTML===""){
-                document.getElementById("positive").insertAdjacentHTML('beforeend',"<h6>Positive Reviews</h6>");
-            }
-
-            document.getElementById("positive").insertAdjacentHTML('beforeend',p);
-            if(review[key].split(" ").length > 10){
-                document.getElementById(divId).insertAdjacentHTML('beforeend',"<span>...</span>")
-            }
-            document.getElementById(divId).addEventListener("click" , function(){
-                this.classList.toggle("show");
-                if(this.classList.contains("show")){
-                    this.innerText= review[key];
-                }
-
-                else{
-                    let prevValue = review[key].split(" ").splice(0,10).join(" ");
-                    this.innerText= prevValue;
-                    if(review[key].split(" ").length > 10){
-                        this.insertAdjacentHTML('beforeend',"<span>...</span>")
-                    }
-                }
-            })
-
-        }else{
-            m = "<div id=";
-            m +=divId;
-           m += " class='review-div mixed'>";
-            m += trimmedText;
-            m += "</div>";
-
-            if(document.getElementById("mixed").innerHTML===""){
-                document.getElementById("mixed").insertAdjacentHTML('beforeend',"<h6>Moderate Reviews</h6>");
-            }
-            document.getElementById("mixed").insertAdjacentHTML('beforeend',m);
-            if(review[key].split(" ").length > 10){
-                document.getElementById(divId).insertAdjacentHTML('beforeend',"<span>...</span>")
-            }
-
-            document.getElementById(divId).addEventListener("click" , function(){
-                this.classList.toggle("show");
-                if(this.classList.contains("show")){
-                    this.innerText= review[key];
-                }
-
-                else{
-                    let prevValue = review[key].split(" ").splice(0,10).join(" ");
-                    this.innerText= prevValue;
-                    console.log()
-                    if(review[key].split(" ").length > 10){
-                        this.insertAdjacentHTML('beforeend',"<span>...</span>")
-                    }
-                }
-            })
-        }
-      });
- 
+      }
+    });
+  });
 }
 
-document.getElementsByClassName("close")[0].addEventListener("click", function(){
-    window.close();
-})
+function images() {
+  console.log("function");
 
-document.getElementsByClassName("cross")[0].addEventListener("click", function(){
-    window.close();
-})
+  document.getElementById("negative").innerHTML = "";
+  document.getElementById("positive").innerHTML = "";
+  document.getElementById("mixed").innerHTML = "";
 
+  document
+    .getElementById("imgBox")
+    .insertAdjacentHTML("beforeend", "<h6>Product Images</h6>");
+
+  let imgs = ``;
+
+  imgData.Images.forEach((img) => {
+    imgs += `<span class="imgDiv"><img src=${img} alt="product" \></span>`;
+  });
+
+  document.getElementById("imgBox").insertAdjacentHTML("beforeend", imgs);
+}
+
+document
+  .getElementsByClassName("close")[0]
+  .addEventListener("click", function () {
+    window.close();
+  });
