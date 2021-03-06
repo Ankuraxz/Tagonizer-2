9f6421596878d40593c335232158957a1571a59e
ANKUR <ankurvermaaxz@gmail.com>
2021-03-06 14:59:12 +0530

    Add Serverless code

--- API/main.py
+++ API/main.py
@@ -11,12 +11,11 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from difflib import SequenceMatcher
 import os
-import uvicorn
-import urllib.request
 
 key1 = os.environ["KEY"]
 ep = os.environ["ENDPOINT"]
 loc = os.environ["LOCATION"]
+
 
 KEY = os.environ["VKEY"]
 ENDPOINT = os.environ["VENDPOINT"]
@@ -121,115 +120,107 @@ def sentiment_analysis_with_opinion_mining_example(documents, client, reviews):
     return j, reviews
 
 
-@app.post('/predict', status_code=status.HTTP_201_CREATED,responses={
-              201: {
-                  "description": "Review Analyzer",
-                  "content": {
-                      "application/json": {
-                          "example": {
-                              "Product_Sentiment":[
-                                  "Overall Sentiment 0 --> Negative  1 --> Positive   2 --> Moderate"
-                              ],
-                              "reviews":{
-                                  "Review Number 0": "Sentiment",
-                                  "Review Number 1": "Sentiment",
-                                  "Review Number 2": "Sentiment",
-                              },
-                              "Tags":{
-                                  "Tag 0": "Sentiment",
-                                  "Tag 1": "Sentiment",
-                                  "Tag 2": "Sentiment"
-                              }
-                          }
-                      }
-                  },
-              },
-          })
-
+@app.post('/predict', status_code=status.HTTP_201_CREATED, responses={
+    201: {
+        "description": "Review Analyzer",
+        "content": {
+            "application/json": {
+                "example": {
+                    "Product_Sentiment": [
+                        "Overall Sentiment 0 --> Negative  1 --> Positive   2 --> Moderate"
+                    ],
+                    "reviews": {
+                        "Review Number 0": "Sentiment",
+                        "Review Number 1": "Sentiment",
+                        "Review Number 2": "Sentiment",
+                    },
+                    "Tags": {
+                        "Tag 0": "Sentiment",
+                        "Tag 1": "Sentiment",
+                        "Tag 2": "Sentiment"
+                    }
+                }
+            }
+        },
+    },
+})
 async def predict(data: data):
     reviews = {}
 
-    if type(data.comments) == list:
-        # print("got_list")
-        # df = processor(data)
-        document = cleaner(data.comments)
-        client = authenticate_client()
-        docSentiment = {}
-        Senti=[]
-        k = 0
-        for ix in document:
-            documents = []
+    document = cleaner(data.comments)
+    client = authenticate_client()
+    docSentiment = {}
+    Senti = []
+    k = 0
+    for ix in document:
+        documents = []
 
-            if len(ix) >= 5000:
-                documents.append(ix[:5000])  # Limiting 5000 chars
-                docSentiment[k], reviews = sentiment_analysis_with_opinion_mining_example(documents, client, reviews)
-            else:
-                documents.append(ix)
-                docSentiment[k], reviews = sentiment_analysis_with_opinion_mining_example(documents, client, reviews)
-            k += 1
-
-        tags = {k: v for k, v in sorted(reviews.items(), key=lambda item: item[1], reverse=True)}
-        keys = tags.keys()
-        reviews.clear()
-
-        s = SequenceMatcher(None)
-        # print(keys)
-        reduntant = set()
-        limit = 0.60
-        for key in keys:
-            s.set_seq2(key)
-            for iy in keys:
-                # wordx = key
-                # wordy = iy
-                s.set_seq1(iy)
-                if key != iy:  # Not The same words
-                    if (s.ratio() >= limit and len(s.get_matching_blocks()) == 2):  # matched word
-                        reduntant.add(iy)
-
-        for ix in reduntant:
-            # print(ix)
-            tags.pop(ix)
-        revResult = {}
-
-        prediction = list(tags.items())
-        docResult = {
-            "Status": "Something Went Wrong"
-        }
-        for t, u in zip(tags.keys(), tags.values()):
-            if u[1] == "negative":
-                revResult[t] = 0
-            elif u[1] == "positive":
-                revResult[t] = 1
-            else:
-                revResult[t] = 2
-        #print(docSentiment)
-        Senti = list(docSentiment.values())
-        if Senti.count(0) > Senti.count(1):
-            overall = 0 #"Negative"
-        elif Senti.count(1) > Senti.count(0):
-            overall = 1 #"Positive"
+        if len(ix) >= 5000:
+            documents.append(ix[:5000])  # Limiting 5000 chars
+            docSentiment[k], reviews = sentiment_analysis_with_opinion_mining_example(documents, client, reviews)
         else:
-            overall = 2 #"Moderate"
+            documents.append(ix)
+            docSentiment[k], reviews = sentiment_analysis_with_opinion_mining_example(documents, client, reviews)
+        k += 1
 
-        if len(revResult.items()) <= 10:
-            docResult = {
-                "Product_Sentiment":overall,
-                "reviews": docSentiment,
-                "tags": revResult
-            }
+    tags = {k: v for k, v in sorted(reviews.items(), key=lambda item: item[1], reverse=True)}
+    keys = tags.keys()
+    reviews.clear()
+
+    s = SequenceMatcher(None)
+    # print(keys)
+    reduntant = set()
+    limit = 0.60
+    for key in keys:
+        s.set_seq2(key)
+        for iy in keys:
+            # wordx = key
+            # wordy = iy
+            s.set_seq1(iy)
+            if key != iy:  # Not The same words
+                if (s.ratio() >= limit and len(s.get_matching_blocks()) == 2):  # matched word
+                    reduntant.add(iy)
+
+    for ix in reduntant:
+        # print(ix)
+        tags.pop(ix)
+    revResult = {}
+
+    prediction = list(tags.items())
+    docResult = {
+        "Status": "Something Went Wrong"
+    }
+    for t, u in zip(tags.keys(), tags.values()):
+        if u[1] == "negative":
+            revResult[t] = 0
+        elif u[1] == "positive":
+            revResult[t] = 1
         else:
-            docResult = {
-                "Product_Sentiment":overall,
-                "reviews": docSentiment,
-                "tags": dict(itertools.islice(revResult.items(), 10))
-            }
-
-        return docResult
-
-
-
+            revResult[t] = 2
+    # print(docSentiment)
+    Senti = list(docSentiment.values())
+    if Senti.count(0) > Senti.count(1):
+        overall = 0  # "Negative"
+    elif Senti.count(1) > Senti.count(0):
+        overall = 1  # "Positive"
     else:
-        return ("PASS A LIST OF REVIEWS")  # -->testing
+        overall = 2  # "Moderate"
+
+    if len(revResult.items()) <= 10:
+        docResult = {
+            "Product_Sentiment": overall,
+            "reviews": docSentiment,
+            "tags": revResult
+        }
+    else:
+        docResult = {
+            "Product_Sentiment": overall,
+            "reviews": docSentiment,
+            "tags": dict(itertools.islice(revResult.items(), 10))
+        }
+
+    return docResult
+
 
 
 def tagger(url, client):
@@ -242,8 +233,6 @@ def tagger(url, client):
                 tags.append(tag.name)
 
     return tags
-
-
 
 
 def url_cleaner(url):
@@ -270,48 +259,37 @@ def url_cleaner(url):
           },
           )
 async def predict_image(data: Vision):
-    if type(data.seller_img) == list and type(data.customer_img) == list:
-        s_tags_set = set()
-        good_images = []
-        c_tags=[]
+    s_tags_set = set()
+    good_images = []
+    c_tags = []
 
-        # client
-        computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+    computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
-        seller_img_resized=[]
-        for ix in data.seller_img:
-            ix = url_cleaner(ix)
-            seller_img_resized.append(ix)
-            s_tags = tagger(ix, computervision_client)
-            for iw in s_tags:
-                iw = iw.lower()
-                s_tags_set.add(iw)
+    seller_img_resized = []
+    for ix in data.seller_img:
+        ix = url_cleaner(ix)
+        seller_img_resized.append(ix)
+        s_tags = tagger(ix, computervision_client)
+        for iw in s_tags:
+            iw = iw.lower()
+            s_tags_set.add(iw)
 
-        for iy in data.customer_img:
-            if "jpg" in iy:
-                iy = url_cleaner(iy)
-                c_tags = tagger(iy, computervision_client)
+    for iy in data.customer_img:
+        if "jpg" in iy:
+            iy = url_cleaner(iy)
+            c_tags = tagger(iy, computervision_client)
 
-            for iz in c_tags:
-                iz = iz.lower()
-                if iz in s_tags_set:
-                    good_images.append(iy)  # iy is image link
-                    break
+        for iz in c_tags:
+            iz = iz.lower()
+            if iz in s_tags_set:
+                good_images.append(iy)  # iy is image link
+                break
 
-        if len(good_images) == 0:
-            good_images = seller_img_resized
+    if len(good_images) == 0:
+        good_images = seller_img_resized
 
+    docResult = {
+        "Images": good_images
+    }
 
-
-        docResult = {
-            "Images": good_images
-        }
-
-        return docResult
-
-    else:
-        return "PASS A LIST OF IMAGES"  # -->testing
-
-
-# if __name__ == '__main__':
-#      uvicorn.run(app, host='127.0.0.1', port = 8000)
+    return docResult
---
+++ vserverless/Tagonizer-image/__init__.py
@@ -0,0 +1,89 @@
+import logging
+import azure.functions as func
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.core.credentials import AzureKeyCredential
+import os
+import json
+
+KEY = os.environ["VKEY"]
+ENDPOINT = os.environ["VENDPOINT"]
+LOCATION = os.environ["LOCATION"]
+
+def tagger(url, client):
+    tags = []
+    # print("===== Tag an image - remote =====")
+    tags_result_remote = client.tag_image(url)
+    if len(tags_result_remote.tags) != 0:
+        for tag in tags_result_remote.tags:
+            if tag.confidence >= 0.45:  # Atleast 45% confidence score
+                tags.append(tag.name)
+
+    return tags
+
+def url_cleaner(url):
+    id = url[49:].split(".")[0]
+    return (url[:49] + id + ".jpg")
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    seller_img = req.params.get('seller_img')
+    if not seller_img:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+
+            seller_img = req_body.get('seller_img')
+
+    customer_img = req.params.get('customer_img')
+    if not customer_img:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            customer_img = req_body.get('customer_img')
+    ####
+    if type(seller_img) ==list and type(customer_img)==list:
+        s_tags_set = set()
+        good_images = []
+        c_tags = []
+
+        computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+        seller_img_resized = []
+        for ix in seller_img:
+            ix = url_cleaner(ix)
+            seller_img_resized.append(ix)
+            s_tags = tagger(ix, computervision_client)
+            for iw in s_tags:
+                iw = iw.lower()
+                s_tags_set.add(iw)
+
+        for iy in customer_img:
+            if "jpg" in iy:
+                iy = url_cleaner(iy)
+                c_tags = tagger(iy, computervision_client)
+
+            for iz in c_tags:
+                iz = iz.lower()
+                if iz in s_tags_set:
+                    good_images.append(iy)
+                    break
+
+        if len(good_images) == 0:
+            good_images = seller_img_resized
+
+        docResult = {
+            "Images": good_images
+        }
+
+        return json.dumps(docResult)
+
+    else:
+        return func.HttpResponse("Failure: Pass Lists/Arrays")
+
+
