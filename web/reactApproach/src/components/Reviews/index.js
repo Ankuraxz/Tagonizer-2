aa59b67d7d4bf35de60f1c488229aae991f47281
vnikonov63 <nikonov.va@yandex.ru>
2021-03-07 09:38:16 +0400

    finished the shrinking of the review, and they are ready right now

--- web/reactApproach/src/components/Reviews/index.js
+++ web/reactApproach/src/components/Reviews/index.js
@@ -37,26 +37,30 @@ const SingleTag = styled.div`
 
 function Reviews() {
   const [dummyState, setDummyState] = useState([
-    { review: "Lorem ipsum dolor sit amet.", status: 1 },
+    { review: "Lorem ipsum dolor sit amet.", status: 1, id: 1 },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis.",
       status: 2,
+      id: 2,
     },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis non magna eget pellentesque. Maecenas.",
       status: 1,
+      id: 3,
     },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim purus sed magna auctor mollis. Nunc at odio ac tortor.",
       status: 3,
+      id: 4,
     },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac tincidunt risus. Praesent lacus urna, porta quis pharetra scelerisque, iaculis commodo risus. Fusce augue odio, accumsan sit amet suscipit id, elementum a nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis venenatis, tellus quis rutrum interdum, quam arcu scelerisque leo, sed posuere magna odio non ante. Nunc.",
       status: 2,
+      id: 5,
     },
     {
       review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rutrum augue sem, vel maximus est blandit ut. In ligula ipsum, ullamcorper facilisis lobortis et, porttitor quis justo. Donec felis velit, pellentesque et neque in, lacinia ullamcorper tellus. Aenean feugiat, enim nec luctus auctor, nulla dolor aliquet dui, vitae blandit est libero quis leo. Nam finibus pretium leo at gravida. In blandit, libero eu vestibulum faucibus, orci nibh molestie mauris, non eleifend nisl urna facilisis nisl. Ut elit turpis, dapibus nec dolor non, mattis malesuada nulla. Suspendisse elementum felis rhoncus, rhoncus nisi vel, congue turpis. Nulla purus odio, ultrices in nisi sed, pellentesque porttitor mi.
@@ -65,39 +69,50 @@ function Reviews() {
     
     Fusce rhoncus mollis mauris eget luctus. Praesent maximus leo sapien. Morbi vitae cursus ipsum. Curabitur a lorem vehicula, varius urna at, elementum sapien. Fusce sed finibus metus. Suspendisse purus turpis, tempus euismod aliquam in, posuere quis velit. Praesent quis mi varius, posuere augue eget, consectetur dui. Mauris elementum convallis eros, id pretium justo malesuada in. Pellentesque leo tellus, posuere et lectus quis, sodales accumsan neque.`,
       status: 2,
+      id: 6,
     },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim purus sed magna auctor mollis. Nunc at odio ac tortor.",
       status: 1,
+      id: 7,
     },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis.",
       status: 1,
+      id: 8,
     },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac tincidunt risus. Praesent lacus urna, porta quis pharetra scelerisque, iaculis commodo risus. Fusce augue odio, accumsan sit amet suscipit id, elementum a nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis venenatis, tellus quis rutrum interdum, quam arcu scelerisque leo, sed posuere magna odio non ante. Nunc.",
       status: 3,
+      id: 9,
     },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis.",
       status: 3,
+      id: 10,
     },
     { review: "Lorem ipsum dolor sit amet.", status: 2 },
     {
       review:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim purus sed magna auctor mollis. Nunc at odio ac tortor.",
       status: 1,
+      id: 11,
     },
   ]);
+
   const [overall, setOverall] = useState({
     positive: 0,
     negative: 0,
     moderate: 0,
   });
+
+  const [shrinked, setShrinked] = useState({});
+
+  // get the count of all of different types
   useEffect(() => {
     dummyState.map(({ status }) => {
       switch (status) {
@@ -125,6 +140,16 @@ function Reviews() {
       }
     });
   }, []);
+
+  // set shrinked up and every is shrinked up by default
+  useEffect(() => {
+    dummyState.map(({ id }) => {
+      setShrinked((prev) => {
+        return { ...prev, [id]: true };
+      });
+    });
+  }, []);
+
   return (
     <div style={{ marginBottom: "30px" }}>
       {overall.negative !== 0 ? (
@@ -134,8 +159,21 @@ function Reviews() {
             .filter((tag) => {
               return tag.status === 1;
             })
-            .map(({ review, status }) => {
-              return <SingleTag status={status}>{review}</SingleTag>;
+            .map(({ review, status, id }) => {
+              return (
+                <SingleTag
+                  status={status}
+                  onClick={() => {
+                    setShrinked((prev) => {
+                      return { ...prev, [id]: !prev[id] };
+                    });
+                  }}
+                >
+                  {shrinked[id] && review.length > 45
+                    ? review.substring(0, 45) + "..."
+                    : review}
+                </SingleTag>
+              );
             })}
         </>
       ) : (
@@ -148,8 +186,21 @@ function Reviews() {
             .filter((tag) => {
               return tag.status === 2;
             })
-            .map(({ review, status }) => {
-              return <SingleTag status={status}>{review}</SingleTag>;
+            .map(({ review, status, id }) => {
+              return (
+                <SingleTag
+                  status={status}
+                  onClick={() => {
+                    setShrinked((prev) => {
+                      return { ...prev, [id]: !prev[id] };
+                    });
+                  }}
+                >
+                  {shrinked[id] && review.length > 45
+                    ? review.substring(0, 45) + "..."
+                    : review}
+                </SingleTag>
+              );
             })}
         </>
       ) : (
@@ -162,8 +213,21 @@ function Reviews() {
             .filter((tag) => {
               return tag.status === 3;
             })
-            .map(({ review, status }) => {
-              return <SingleTag status={status}>{review}</SingleTag>;
+            .map(({ review, status, id }) => {
+              return (
+                <SingleTag
+                  status={status}
+                  onClick={() => {
+                    setShrinked((prev) => {
+                      return { ...prev, [id]: !prev[id] };
+                    });
+                  }}
+                >
+                  {shrinked[id] && review.length > 45
+                    ? review.substring(0, 45) + "..."
+                    : review}
+                </SingleTag>
+              );
             })}
         </>
       ) : (
